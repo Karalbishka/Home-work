56a7ec295160311e8623be37d438db90d850897a
Karalbishka <106267760+Karalbishka@users.noreply.github.com>
2022-12-16 10:52:23 +0300

    Add files via upload

---
+++ seminar 1.4.py
@@ -0,0 +1,10 @@
+a = int(input())
+if a == 1:
+    print('(x > 0; y > 0)')
+elif a == 2:
+    print('(x < 0; y > 0)')
+elif a == 3:
+    print('(x < 0; y < 0)')
+elif a == 4:
+    print('(x > 0; y < 0)')
+
